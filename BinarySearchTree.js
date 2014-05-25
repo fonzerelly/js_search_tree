20387ce8c47e470bf403177109b44c6360fcde56
choerauf <christian.hoerauf@inserteffect.com>
2014-05-25 21:28:23 +0200

    added size

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,6 +1,17 @@
-function T() {
+function size (aTree) {
+    return aTree.size;
+}
 
+function T(value, leftTree, rightTree) {
+    this.value = value;
+    this.leftTree = leftTree || new E();
+    this.rightTree = rightTree || new E();
+    this.size = 1 + size(this.leftTree) + size(this.rightTree);
 }
+
 function E () {
+    this.size = 0;
 }
+
 E.prototype = new T();
+
--- Spec_BinarySearchTree.js
+++ Spec_BinarySearchTree.js
@@ -1,13 +1,16 @@
 /*global describe, it, expect, beforeEach,  _, load, java, nextGeneration*/
 
 describe ("BinarySearchTree", function () {
+
     describe ("#Constructors", function () {
+
         describe ("#Tree", function () {
             it ("should be a tree", function () {
                 var result = new T();
                 expect(result instanceof T).toBe(true);
             });
         });
+
         describe ("#EmtpyTree", function () {
             it ("should be an empty tree", function () {
                 var result = new E();
@@ -15,6 +18,27 @@ describe ("BinarySearchTree", function () {
                 expect(result instanceof T).toBe(true);
 
             });
+        });
+
+    });
+
+    describe ("#size", function () {
+
+        it ("should return zero for empty tree", function () {
+            expect(size(new E())).toBe(0);
+        });
+
+        it ("should return one for a tree without subtrees", function () {
+            expect(size(new T())).toBe(1);
+        });
+
+        it ("should return the size of the tree, including its subtree sizes", function () {
+            tree = new T(
+                0,
+                new T(-1),
+                new T(1)
+            );
+            expect(size(tree)).toBe(3);
         });
     });
 });
